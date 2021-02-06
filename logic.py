8518765c0fb0216f04b1d424210793da8e14dfca
Alejo Gomez <alego17@gmail.com>
2021-02-06 12:27:20 -0500

    se agregó /about #9

---
+++ logic.py
@@ -0,0 +1,7 @@
+def get_about_this(VERSION):
+    response = (
+        f"Simple Expenses Bot (pyTelegramBot) v{VERSION}"
+        "\n\n"
+        "Desarrollado por Alejo Gómez <alego17@gmail.com>"
+    )   
+    return response
