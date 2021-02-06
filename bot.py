a1683dc5e2ca267fd333b6cf7c20a6b15201a56e
Alejo Gomez <alego17@gmail.com>
2021-02-06 12:14:42 -0500

    se implementó la persistencia de los datos #8

--- bot.py
+++ bot.py
@@ -4,7 +4,13 @@ from config import bot
 import config
 from time import sleep
 import re
-import logic 
+import logic
+import database.db as db
+
+#########################################################
+
+if __name__ == '__main__':
+    db.Base.metadata.create_all(db.engine)
 
 #########################################################
 
@@ -17,6 +23,35 @@ def on_command_start(message):
 @bot.message_handler(commands=['help'])
 def on_command_help(message):
     pass
+
+@bot.message_handler(commands=['about'])
+def on_command_about(message):
+    pass
+
+@bot.message_handler(regexp=r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_earn_money(message):
+    pass
+
+@bot.message_handler(regexp=r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_spend_money(message):
+    pass
+
+@bot.message_handler(regexp=r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_earnings(message):
+    pass
+
+@bot.message_handler(regexp=r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_spendings(message):
+    pass
+
+@bot.message_handler(regexp=r"^(obtener saldo|s)$")
+def on_get_balance(message):
+    pass
+
+@bot.message_handler(regexp=r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$")
+def on_remove_record(message):
+    pass
+
     
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
